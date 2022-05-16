c7c2caf82fb730403fff949e5d7d2b9db236f6a6
Yi Zhang <z@wtmsb.io>
2022-05-16 06:05:07 -0400

    refactor per #21, fix typo (can't get arinc right)

--- source/models/airac.ts
+++ source/models/airac.ts
@@ -1,40 +1,40 @@
 interface Route {
-    identifier: string
-    route_segments: [RouteSegment]
+	identifier: string;
+	route_segments: [RouteSegment];
 }
 
 interface RouteSegment {
-    sequence_number: number
-    waypoint: Waypoint
-    distance_to: number
-    distance_from: number
+	sequence_number: number;
+	waypoint: Waypoint;
+	distance_to: number;
+	distance_from: number;
 }
 
 interface Waypoint {
-    point: NavAid | NavFix | TransferOfControlPoint
+	point: NavAid | NavFix | TransferOfControlPoint;
 }
 
 enum NavAidType {
-    VOR,
-    NDB
+	VOR,
+	NDB
 }
 
 interface NavAid {
-    identifier: string
-    type: NavAidType
+	identifier: string;
+	type: NavAidType;
 }
 
 enum NavFixType {
-    INTERSECTION,
-    FIX
+	INTERSECTION,
+	FIX
 }
 
 interface NavFix {
-    identifier: string
-    nav_fix_type: NavFixType
+	identifier: string;
+	nav_fix_type: NavFixType;
 }
 
 interface TransferOfControlPoint {
-    facility_1: string
-    facility_2: string
+	facility_1: string;
+	facility_2: string;
 }
